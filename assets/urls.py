c9e2a1cf377bd563e2f2f203ddfd533ecec2e354
guog <k.guogao@outlook.com>
2018-03-15 19:10:54 +0800

    2018/03/15

--- assets/urls.py
+++ assets/urls.py
@@ -1,6 +1,4 @@
 
-from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
 from assets import views
 
--- c13/urls.py
+++ c13/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
 from c13 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.IndexViews),
     url(r'^personal/', views.personal, name='personal'),
     url(r'^accounts/logout/',views.logout,name='logout'),
